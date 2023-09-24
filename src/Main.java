e9808e7169ea402861faab328ce6eb5d9e6fc332
Санёк Виничук <sashavinichuk@gmail.com>
2023-09-24 21:23:34 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -41,6 +41,7 @@ public class Main {
             System.out.println("Answere " + res);
         }
 
+        System.out.println("Next task");
         int option;
         do {
             int fnum;
